1e3424e5df5cd6a9a78e6401cee94e9c3a23abb8
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-03-15 19:17:16 +0300

    feat: modify package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,113 @@
-const config = {
-	
-}
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = (env, options) => {
-	let production = options.mode === 'production';
-	config.devtool = production ? false : 'eval-sourcemap';
-	
-	return config;
-}
+const PRODUCTION = 'production';
+const DEVELOPMENT = 'development';
+
+const env = process.env.NODE_ENV;
+const isProduction = env && env.trim() === PRODUCTION;
+const isDevelopment = !isProduction;
+const distPath = path.join(__dirname, '/public');
+
+const config = {
+    mode: isProduction ? PRODUCTION : DEVELOPMENT,
+
+    entry: {
+        app: './src/index.js',
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: distPath,
+    },
+
+    devtool: isDevelopment ? 'source-map' : 'none',
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{ loader: 'babel-loader' }],
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                isProduction ? require('cssnano') : () => {},
+                                require('autoprefixer')({
+                                    browsers: ['last 2 versions'],
+                                }),
+                            ],
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                        limit: 8192,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.json/,
+                use: [{ loader: 'json-loader' }],
+            },
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: path.resolve(__dirname, './src/index.html'),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+
+    optimization: isProduction ? {
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: false,
+                uglifyOptions: {
+                compress: {
+                    inline: false,
+                    drop_console: true,
+                },
+                },
+            }),
+        ],
+    } : {},
+
+    devServer: {
+        contentBase: distPath,
+        port: 5000,
+        compress: true,
+        open: true,
+        hot: true,
+    },
+};
+
+module.exports = config;

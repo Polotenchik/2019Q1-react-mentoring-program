0ba18efa6057f421f1d5bbdc053cf804d8eb8737
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-03-22 17:25:53 +0300

    docs: add main.scss file

--- src/components/react-create-element.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CreateElementComponent() {
-    return (React.createElement(
-        'h1',
-        null,
-        'Hello world! I was created with React.createElement'
-    ));
-} 

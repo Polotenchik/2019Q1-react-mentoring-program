d77ae0dcb9f7d8f95f443dc425f873d52018f4b1
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-04-13 09:39:49 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './main.scss';
+import { Provider } from 'react-redux';
 import ErrorBoundaryContainer from './containers/ErorrBoundary/ErrorBoundaryContainer';
 import App from './App';
+import store from './store';
 
 
 ReactDOM.render(
-    <ErrorBoundaryContainer>
-        <App />
-    </ErrorBoundaryContainer>,
+    <Provider store={ store }>
+        <ErrorBoundaryContainer>
+            <App />
+        </ErrorBoundaryContainer>
+    </Provider>,
     document.getElementById('greeting')
 );

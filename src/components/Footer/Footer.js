299e94c60f7be72838213f33517a904b57dc1ec1
Nikita Hapeyeu <Mikita_Hapeyeu@epam.com>
2019-05-23 19:51:46 +0300

    feat: add styled-components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Logo } from '../index';
+import styled from 'styled-components';
+
+const ContainerFooter = styled.footer`
+    height: 60px;
+    width: 1150px;
+    background: #434343;
+    flex: 0 0 auto;
+    margin: 0 auto;
+`;
 
 const Footer = () => (
-    <footer className={'footer'}>
-        <Logo />
-    </footer>
+     <ContainerFooter>
+         <Logo />
+     </ContainerFooter>
 );
+
+
+// const Footer = () => (
+//     <footer className={'footer'}>
+//         <Logo />
+//     </footer>
+// );
 
 export default Footer;
